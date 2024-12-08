9ed4ae6ff70cdc713f5a19c89aab05fa7f910566
KhacThien88 <KTeightop1512@gmail.com>
2024-12-08 15:41:01 +0700

    update scripts of VM

--- modules/azurerm_linux_virtual_machine/main.tf
+++ modules/azurerm_linux_virtual_machine/main.tf
@@ -20,6 +20,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "22_04-lts"
     version   = "latest"
   }
+  custom_data = <<-EOT
+              #!/bin/bash
+              echo "root:YourRootPassword123!" | chpasswd
+              sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+              systemctl restart sshd 
+            EOT
   tags = {
     environment = var.environment
   }
